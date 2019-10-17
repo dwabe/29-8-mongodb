759c6b5e677571d89cbbcccd4c0b622392cb9ab2
Bartek Błęcki <bartek@blecki.pl>
2019-10-17 18:45:35 +0200

    publish to heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/nodeappdatabase', {
+mongoose.connect('mongodb+srv://barble:<123qwe>@cluster0-laqmc.mongodb.net/test?retryWrites=true&w=majority', {
     useMongoClient: true
 });
 
